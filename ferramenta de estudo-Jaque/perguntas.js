2dde1cc06480b885e301194895ace53cb7d120b0
ProfJaque2023 <145795123+ProfJaque2023@users.noreply.github.com>
2024-11-08 10:07:46 -0300

    Add files via upload

---
+++ ferramenta de estudo-Jaque/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amor em Inglês?',
+    'Amor em ingles é Love (love)'
+)
