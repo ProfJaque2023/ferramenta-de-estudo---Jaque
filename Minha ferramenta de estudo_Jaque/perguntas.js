88e63c8a297cb7455666bf219680a697a9f967e9
ProfJaque2023 <145795123+ProfJaque2023@users.noreply.github.com>
2024-11-19 08:10:34 -0300

    Add files via upload

---
+++ Minha ferramenta de estudo_Jaque/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é GitHub?',
+    'O Github é um repositorio de programação'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a diferença de circulo e circunferência?',
+    'Circulo é o preenchimento da circunferência e circunferência e o contorno'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz pessoas em Inglês?',
+    'pessoas em ingles é people (pipou)'
+)
